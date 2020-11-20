b9c3923875437a3cfa60d7d1ee1a488ba0edce71
nowxel <58902795+nowxel@users.noreply.github.com>
2020-11-20 16:52:47 +0200

    Create body.dart

---
+++ body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/components/body.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+
+}
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme
+          .of(context)
+          .primaryColor,
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+   AppBar buildAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        iconSize: 30.0,
+        color: Colors.white,
+      ),
+      title: Text('Dresses',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFFFEF9EB),
+        ),
+      ),
+      elevation: 0.0,
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.search)
+        ),
+        IconButton(
+          icon: Icon(Icons.search),
+        )
+      ],
+    );
+   }
+}
